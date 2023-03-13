a9ffe4cdd844da269c873a3369e8aa4988e1842d
Amrut <tusharkakad18@gmail.com>
2023-03-13 11:53:07 +0530

    first commit

---
+++ Simple.java
@@ -0,0 +1,22 @@
+class Simple
+{
+	int a,b;
+	Simple()
+	{
+		this.a=0;
+		this.b=0;
+	}
+	Simple(int a,int b)
+	{
+		this.a=a;
+		this.b=b;
+	}
+	static void add(Simple simple1,Simple simple2)
+	{
+		Simple simple3=new Simple(0,0);
+		Simple simple4=new Simple(0,0);
+		Simple simple5=new Simple(0,0);
+		Simple simple6=new Simple(0,0);
+	}
+}
+
